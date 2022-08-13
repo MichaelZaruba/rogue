387796306121059023c2c7a7095ba66a8ff91310
Big_LoseR <ilya.demidov.2002@mail.ru>
2022-08-13 20:57:38 +0300

    probably chest

--- Assets/_Scripts/Chest.cs
+++ Assets/_Scripts/Chest.cs
@@ -2,5 +2,33 @@
 
 public class Chest : MonoBehaviour
 {
-    // TODO chest logic
+    private bool _playerIsNear;
+    private bool _isOpenedChest = false;
+    public Animator animator;
+
+    private void Update()
+    {
+        if (_isOpenedChest)
+        {
+            animator.Play("Empty");
+            return;
+        }
+        if (!_playerIsNear)
+            return;
+    
+        if (Input.GetKeyDown(KeyCode.E))
+            {
+            _isOpenedChest = true;
+            animator.Play("Empty");
+            }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            animator.Play("Trigger");
+            _playerIsNear = true;
+        }
+    }
 }
